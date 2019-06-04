2d603f8eb362db24698cbcd0ac5fb3633a6102b1
ElizabethCO25 <maria250593@gmail.com>
2019-06-04 13:03:36 -0500

    Agregando evento OnClick

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-/* Acá va tu código */
+function hacer_clickIngresar()
+{
+    alert("Ingresaste una contraseña");
+}
+
+let ObtenerContraseña = function(){
+    let contraseña = document.getElementById("contraseña").value;
+    console.log("La contraseña ingresada es: " + contraseña);
+}
