5aafad77bf8460c74ff07a64314a0d3a8b442dc5
ElizabethCO25 <maria250593@gmail.com>
2019-06-13 10:10:31 -0500

    Detalles finales

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,18 @@
 window.cipher = {
   encode: (offset, string) => {
   //Obteniendo mensaje a encriptar:
-   let mensajeEncriptado = " ";
+   let mensajeEncriptado = "";
    for (let i = 0; i < string.length; i++) {
        let letra = string[i];
-       if (letra.match(/[a-z,0-9]/i)) {
+       if (letra.match(/[a-z,0-9, ]/i)) {
            if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
                let texto = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
                mensajeEncriptado += String.fromCharCode(texto);
            } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-               texto = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+               let texto = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
                mensajeEncriptado += String.fromCharCode(texto);
            } else if (string.charCodeAt(i) >= 48 && string.charCodeAt(i) <= 57) {
-              texto = (string.charCodeAt(i) - 48 + parseInt(offset)) % 26 + 48;
+              let texto = (string.charCodeAt(i) - 48 + parseInt(offset)) % 26 + 48;
               mensajeEncriptado += String.fromCharCode(texto);
            }
        } else {
@@ -25,18 +25,18 @@ window.cipher = {
 
   decode: (offset, string) => {
   //Obteniendo mensaje a desencriptar:
-    let mensajeDesencriptado = " ";
+    let mensajeDesencriptado = "";
     for (let i = 0; i < string.length; i++) {
         let letra2 = string[i];
-        if (letra2.match(/[a-z,0-9]/i)) {
+        if (letra2.match(/[a-z,0-9, ]/i)) {
             if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
                 let texto2 = (string.charCodeAt(i) - 65 - parseInt(offset)) % 26 + 65;
                 mensajeDesencriptado += String.fromCharCode(texto2);
             }else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-                  let texto2 = (string.charCodeAt(i) - 97 - parseInt(offset)) % 26 + 97;
+                let texto2 = (string.charCodeAt(i) - 97 - parseInt(offset)) % 26 + 97;
                   mensajeDesencriptado += String.fromCharCode(texto2);
             }else if (string.charCodeAt(i) >= 48 && string.charCodeAt(i) <= 57) {
-                   texto2 = (string.charCodeAt(i) - 48 - parseInt(offset)) % 26 + 48;
+                let texto2 = (string.charCodeAt(i) - 48 - parseInt(offset)) % 26 + 48;
                    mensajeDesencriptado += String.fromCharCode(texto2);
             }
         }
--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ eleccionOpcion2.addEventListener("click",() =>
     document.getElementById("pantalla1").classList.add("ocultar");
 } );
 
+
 //Restricción de ingreso de sólo números en el input de Ofset
 function soloNumeros(e){
   key=e.keyCode || e.which;
@@ -65,6 +66,30 @@ function soloNumeros(e){
       return false;
     }
 }
+//BOTONES RESET:
+// Función botón RESET1:
+const clickReset1 = document.getElementById("bReset1");
+const textoEntrada1 = document.getElementById("textArea1_1");
+const textoSalida1 = document.getElementById("textArea1_2");
+const offsetEnc = document.getElementById("offset");
+clickReset1.addEventListener("click",() =>
+{
+    textoEntrada1.value = "";
+    textoSalida1.value = "";
+    offsetEnc.value = "";
+} );
+// Función botón RESET2:
+const clickReset2 = document.getElementById("bReset2");
+const textoEntrada2 = document.getElementById("textArea2_1");
+const textoSalida2 = document.getElementById("textArea2_2");
+const offsetDes = document.getElementById("offset2");
+clickReset2.addEventListener("click",() =>
+{
+    textoEntrada2.value = "";
+    textoSalida2.value = "";
+    offsetDes.value = "";
+} );
+
 
 //BOTONES SALIR:
 // Función botón SALIR1:
@@ -106,17 +131,17 @@ eleccionRegresar.addEventListener("click",() =>
 //ENCRIPTANDO:
 //Texto a encriptar:
   const clickEncriptar = document.getElementById("bHacerEncriptar");
-  clickEncriptar.addEventListener("click", function() {
-      let textoEncriptado = document.getElementById("textArea1.1").value;
-      let numOffset = document.getElementById("Offset").value;
-      document.getElementById("textArea1.2").innerHTML = window.cipher.encode(numOffset, textoEncriptado);
+  clickEncriptar.addEventListener("click", () =>  {
+      let textoEncriptado = document.getElementById("textArea1_1").value;
+      let numOffset = document.getElementById("offset").value;
+      document.getElementById("textArea1_2").innerHTML = cipher.encode(numOffset, textoEncriptado);
   });
 
 //DESENCRIPTANDO:
 //Texto a desencriptar:
   const clickDesencriptar = document.getElementById("bHacerDesencriptar");
-  clickDesencriptar.addEventListener("click", function() {
-      let textoDesencriptado = document.getElementById("textArea2.1").value;
-      let numOffset2 = document.getElementById("Offset2").value;
-      document.getElementById("textArea2.2").innerHTML = window.cipher.decode(numOffset2, textoDesencriptado);
+  clickDesencriptar.addEventListener("click",() => {
+      let textoDesencriptado = document.getElementById("textArea2_1").value;
+      let numOffset2 = document.getElementById("offset2").value;
+      document.getElementById("textArea2_2").innerHTML = cipher.decode(numOffset2, textoDesencriptado);
   });
